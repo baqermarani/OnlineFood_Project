4d62df09ce8f86077e3a063a79f6390de7169bcd
baqer marani <60050143+baqermarani@users.noreply.github.com>
2021-02-05 02:30:33 +0330

    Add project files.

---
+++ CoffeeShop/Models/OrderModels.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace CoffeeShop.Models
+{
+    public class Order
+    {
+        public int ID { get; set; }
+
+        public string Username { get; set; }
+
+        public string Phonenumber { get; set; }
+
+        public string Address { get; set; }
+
+        public int Totalprice { get; set; }
+
+        public bool Ispayed { get; set; }
+
+        public bool Isdelivered { get; set; }
+
+        public virtual ICollection<OrderDetails> OrderDetails { get; set; }
+
+
+    }
+}
---
+++ CoffeeShop/Models/shoppingcartViewModels.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CoffeeShop.Models
+{
+    public class shoppingcartViewModels
+    {
+        [Key]
+        public int ID { get; set; }
+        public Product Product { get; set; }
+        [Display(Name ="تعداد")]
+        public int quantity { get; set; }
+    }
+}
